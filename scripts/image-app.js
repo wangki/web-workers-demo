1b173b4b47458361c5e67b2ddeb314857d8e29b5
Dennis Lee <wonkeun6@gmail.com>
2017-06-04 17:40:23 +0900

    no message

--- scripts/image-app.js
+++ scripts/image-app.js
@@ -1,3 +1,5 @@
+
+// importScripts('worker.js');
 (function(){
   // http://stackoverflow.com/questions/10906734/how-to-upload-image-into-html5-canvas
   var original;
@@ -5,6 +7,8 @@
   imageLoader.addEventListener('change', handleImage, false);
   var canvas = document.querySelector('#image');
   var ctx = canvas.getContext('2d');
+  var imageWorker = new Worker('scripts/worker.js');
+
 
   function handleImage(e){
     var reader = new FileReader();
@@ -43,18 +47,29 @@
     // Hint! This is where you should post messages to the web worker and
     // receive messages from the web worker.
 
-    length = imageData.data.length / 4;
-    for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
-      r = imageData.data[i * 4 + 0];
-      g = imageData.data[i * 4 + 1];
-      b = imageData.data[i * 4 + 2];
-      a = imageData.data[i * 4 + 3];
-      pixel = manipulate(type, r, g, b, a);
-      imageData.data[i * 4 + 0] = pixel[0];
-      imageData.data[i * 4 + 1] = pixel[1];
-      imageData.data[i * 4 + 2] = pixel[2];
-      imageData.data[i * 4 + 3] = pixel[3];
-    }
+    imageWorker.postMessage({'imageData' : imageData, 'type': type});
+
+    imageWorker.onmessage = function (e) {
+      // once the worker sends back
+      //greys out
+      toggleButtonsAbledness();
+
+      var image = e.data;
+      if(image) return ctx.putImageData(e.data,0,0);
+      console.log("No manipulated image returned")
+
+    };
+    imageWorker.onerror = function (e) {
+      function WorkerException(message) {
+          this.name = "WorkerException";
+          this.message = message;
+      }
+      throw new WorkerException("error")
+    };
+
+
+
+
     toggleButtonsAbledness();
     return ctx.putImageData(imageData, 0, 0);
   };
@@ -62,6 +77,8 @@
   function revertImage() {
     return ctx.putImageData(original, 0, 0);
   }
+
+
 
   document.querySelector('#invert').onclick = function() {
     manipulateImage("invert");
